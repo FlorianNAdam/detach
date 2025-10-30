b4732274c1c0dea7501dd906a75a204a12bc567a
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-10-30 19:28:48 +0100

    package with nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -9,6 +13,7 @@
       self,
       flake-utils,
       nixpkgs,
+      naersk,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -16,8 +21,20 @@
         pkgs = (import nixpkgs) {
           inherit system;
         };
+
+        naersk-lib = pkgs.callPackage naersk { };
+
+        detach = naersk-lib.buildPackage {
+          pname = "detach";
+          src = ./.;
+        };
       in
       {
+        packages = {
+          inherit detach;
+          default = detach;
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
